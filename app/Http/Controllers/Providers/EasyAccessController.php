a1defba1b246366cf0b39b819465f4974a8e5f95
davsong01 <davsong16@gmail.com>
2024-02-10 07:33:59 +0100

    variation fixes

--- app/Http/Controllers/Providers/EasyAccessController.php
+++ app/Http/Controllers/Providers/EasyAccessController.php
@@ -27,9 +27,6 @@ class EasyAccessController extends Controller
             $variations = $variations['content']['variations'] ?? $variations['content']['varations'];
 
             foreach ($variations as $variation) {
-                // if(in_array($variation[
-                    'variation_code'], $existingVariations)){
-                // }else{
                 Variation::updateOrCreate([
                     'product_id' => $product['id'],
                     'category_id' => $product['category_id'],
@@ -60,7 +57,6 @@ class EasyAccessController extends Controller
             return false;
         }
     }
-
 
     public function esayAccessProductVariations(){
         $variations = [
@@ -93,140 +89,163 @@ class EasyAccessController extends Controller
                     [
                         'variation_code' => 53, 
                         'fixedPrice' => 'Yes', 
-                        'name' => '3GB (SME) - N771 30days'],
-                        [
-                            'variation_code' => 54, 'fixedPrice' => 'Yes', 'name' => '5GB (SME) - N1285 30days'
-                    ', varition_amount' =>],
-                [
-                    'variation_code' => 91, 'fixedPrice' => 'Yes', 'name' => '10GB (SME) - N2570 30days'
-             ', varition_amount' =>],
-                [
-                    'variation_code' => 177, 'fixedPrice' => 'Yes', 'name' => '50MB (CG_LITE) - N57 30days'
-             ', varition_amount' =>],
-                [
-                    'variation_code' => 178, 'fixedPrice' => 'Yes', 'name' => '150MB (CG_LITE) - N67 30days'
-             ', varition_amount' =>],
-                [
-                    'variation_code' => 179, 'fixedPrice' => 'Yes', 'name' => '250MB (CG_LITE) - N76 30days'
-             ', varition_amount' =>],
-                [
-                    'variation_code' => 180, 'fixedPrice' => 'Yes', 'name' => '500MB (CG_LITE) - N131 30days'
-             ', varition_amount' =>],
-                [
-                    'variation_code' => 181, 'fixedPrice' => 'Yes', 'name' => '1GB (CG_LITE) - N256 30days', ,'varition_amount' =>],
-                 [
-                    'variation_code' => 182, 'fixedPrice' => 'Yes', 'name' => '2GB (CG_LITE) - N512 30days'
-                 ', varition_amount' =>],
-                 [
-                    'variation_code' => 183, 'fixedPrice' => 'Yes', 'name' => '3GB (CG_LITE) - N768 30days' ', varition_amount' =>],
-                 [
-                    'variation_code' => 184, 'fixedPrice' => 'Yes', 'name' => '5GB (CG_LITE) - N1280 30days'
-                 ', varition_amount' =>],
-                 [
-                    'variation_code' => 185, 'fixedPrice' => 'Yes', 'name' => '10GB (CG_LITE) - N2560 30days'
-                 ', varition_amount' =>],
-                 [
-                    'variation_code' => 186, 'fixedPrice' => 'Yes', 'name' => '12GB (CG_LITE) - N3072 30days'
-                 ', varition_amount' =>],
-                 [
-                    'variation_code' => 187, 'fixedPrice' => 'Yes', 'name' => '15GB (CG_LITE) - N3840 30days', ,'varition_amount' =>],
-                  [
-                    'variation_code' => 188, 'fixedPrice' => 'Yes', 'name' => '20GB (CG_LITE) - N5120 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 189, 'fixedPrice' => 'Yes', 'name' => '25GB (CG_LITE) - N555688000 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 190, 'fixedPrice' => 'Yes', 'name' => '30GB (CG_LITE) - N666825600 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 191, 'fixedPrice' => 'Yes', 'name' => '40GB (CG_LITE) - N889100800 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 192, 'fixedPrice' => 'Yes', 'name' => '50GB (CG_LITE) - N1111376000 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 193, 'fixedPrice' => 'Yes', 'name' => '60GB (CG_LITE) - N1333651200 30days'
-                 ', varition_amount' =>],
-                  [
-                    'variation_code' => 194, 'fixedPrice' => 'Yes', 'name' => '70GB (CG_LITE) - N1555926400 30days', ,'varition_amount' =>],
-                   [
-                    'variation_code' => 195, 'fixedPrice' => 'Yes', 'name' => '75GB (CG_LITE) - N1667064000 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 196, 'fixedPrice' => 'Yes', 'name' => '80GB (CG_LITE) - N1778201600 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 197, 'fixedPrice' => 'Yes', 'name' => '90GB (CG_LITE) - N2000476800 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 198, 'fixedPrice' => 'Yes', 'name' => '100GB (CG_LITE) - N2222752000 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 125, 'fixedPrice' => 'Yes', 'name' => '50MB (CG) - N57 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 126, 'fixedPrice' => 'Yes', 'name' => '150MB (CG) - N67 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 127, 'fixedPrice' => 'Yes', 'name' => '250MB (CG) - N76 30days'
-                 ', varition_amount' =>],
-                   [
-                    'variation_code' => 45, 'fixedPrice' => 'Yes', 'name' => '500MB (CG) - N132 30days', ,'varition_amount' =>],
-                    [
-                        'variation_code' => 46, 'fixedPrice' => 'Yes', 'name' => '1GB (CG) - N261 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 47, 'fixedPrice' => 'Yes', 'name' => '2GB (CG) - N522 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 48, 'fixedPrice' => 'Yes', 'name' => '3GB (CG) - N783 30days' ', varition_amount' =>],
-                    [
-                        'variation_code' => 49, 'fixedPrice' => 'Yes', 'name' => '5GB (CG) - N1305 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 88, 'fixedPrice' => 'Yes', 'name' => '10GB (CG) - N2610 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 89, 'fixedPrice' => 'Yes', 'name' => '12GB (CG) - N3132 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 90, 'fixedPrice' => 'Yes', 'name' => '15GB (CG) - N3915 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 141, 'fixedPrice' => 'Yes', 'name' => '20GB (CG) - N5220 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 142, 'fixedPrice' => 'Yes', 'name' => '25GB (CG) - N563187000 30days'
-                 ', varition_amount' =>],
-                    [
-                        'variation_code' => 143, 'fixedPrice' => 'Yes', 'name' => '30GB (CG) - N675824400 30days', 'varition_amount' =>],
+                        'name' => '3GB (SME) - 30days',
+                        'variation_code' => 771
+                    ],
+                    //         [
+                    //             'variation_code' => 54, 'fixedPrice' => 'Yes', 'name' => '5GB (SME) - N1285 30days'
+                    //         ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 91, 'fixedPrice' => 'Yes', 'name' => '10GB (SME) - N2570 30days'
+                    //  ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 177, 'fixedPrice' => 'Yes', 'name' => '50MB (CG_LITE) - N57 30days'
+                    //  ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 178, 'fixedPrice' => 'Yes', 'name' => '150MB (CG_LITE) - N67 30days'
+                    //  ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 179, 'fixedPrice' => 'Yes', 'name' => '250MB (CG_LITE) - N76 30days'
+                    //  ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 180, 'fixedPrice' => 'Yes', 'name' => '500MB (CG_LITE) - N131 30days'
+                    //  ', varition_amount' =>],
+                    //     [
+                    //         'variation_code' => 181, 'fixedPrice' => 'Yes', 'name' => '1GB (CG_LITE) - N256 30days', ,'varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 182, 'fixedPrice' => 'Yes', 'name' => '2GB (CG_LITE) - N512 30days'
+                    //      ', varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 183, 'fixedPrice' => 'Yes', 'name' => '3GB (CG_LITE) - N768 30days' ', varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 184, 'fixedPrice' => 'Yes', 'name' => '5GB (CG_LITE) - N1280 30days'
+                    //      ', varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 185, 'fixedPrice' => 'Yes', 'name' => '10GB (CG_LITE) - N2560 30days'
+                    //      ', varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 186, 'fixedPrice' => 'Yes', 'name' => '12GB (CG_LITE) - N3072 30days'
+                    //      ', varition_amount' =>],
+                    //      [
+                    //         'variation_code' => 187, 'fixedPrice' => 'Yes', 'name' => '15GB (CG_LITE) - N3840 30days', ,'varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 188, 'fixedPrice' => 'Yes', 'name' => '20GB (CG_LITE) - N5120 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 189, 'fixedPrice' => 'Yes', 'name' => '25GB (CG_LITE) - N555688000 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 190, 'fixedPrice' => 'Yes', 'name' => '30GB (CG_LITE) - N666825600 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 191, 'fixedPrice' => 'Yes', 'name' => '40GB (CG_LITE) - N889100800 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 192, 'fixedPrice' => 'Yes', 'name' => '50GB (CG_LITE) - N1111376000 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 193, 'fixedPrice' => 'Yes', 'name' => '60GB (CG_LITE) - N1333651200 30days'
+                    //      ', varition_amount' =>],
+                    //       [
+                    //         'variation_code' => 194, 'fixedPrice' => 'Yes', 'name' => '70GB (CG_LITE) - N1555926400 30days', ,'varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 195, 'fixedPrice' => 'Yes', 'name' => '75GB (CG_LITE) - N1667064000 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 196, 'fixedPrice' => 'Yes', 'name' => '80GB (CG_LITE) - N1778201600 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 197, 'fixedPrice' => 'Yes', 'name' => '90GB (CG_LITE) - N2000476800 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 198, 'fixedPrice' => 'Yes', 'name' => '100GB (CG_LITE) - N2222752000 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 125, 'fixedPrice' => 'Yes', 'name' => '50MB (CG) - N57 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 126, 'fixedPrice' => 'Yes', 'name' => '150MB (CG) - N67 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 127, 'fixedPrice' => 'Yes', 'name' => '250MB (CG) - N76 30days'
+                    //      ', varition_amount' =>],
+                    //        [
+                    //         'variation_code' => 45, 'fixedPrice' => 'Yes', 'name' => '500MB (CG) - N132 30days', ,'varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 46, 'fixedPrice' => 'Yes', 'name' => '1GB (CG) - N261 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 47, 'fixedPrice' => 'Yes', 'name' => '2GB (CG) - N522 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 48, 'fixedPrice' => 'Yes', 'name' => '3GB (CG) - N783 30days' ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 49, 'fixedPrice' => 'Yes', 'name' => '5GB (CG) - N1305 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 88, 'fixedPrice' => 'Yes', 'name' => '10GB (CG) - N2610 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 89, 'fixedPrice' => 'Yes', 'name' => '12GB (CG) - N3132 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 90, 'fixedPrice' => 'Yes', 'name' => '15GB (CG) - N3915 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 141, 'fixedPrice' => 'Yes', 'name' => '20GB (CG) - N5220 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 142, 'fixedPrice' => 'Yes', 'name' => '25GB (CG) - N563187000 30days'
+                    //      ', varition_amount' =>],
+                    //         [
+                    //             'variation_code' => 143, 'fixedPrice' => 'Yes', 'name' => '30GB (CG) - N675824400 30days', 'varition_amount' =>],
+                    //          [
+                    //             'variation_code' => 144, 'fixedPrice' => 'Yes', 'name' => '40GB (CG) - N901099200 30days', 'varition_amount' =>],
+                    //          [
+                    //             'variation_code' => 145, 'fixedPrice' => 'Yes', 'name' => '50GB (CG) - N1126374000 30days', 'varition_amount' =>],
+                    //          [
+                    //             'variation_code' => 146, 'fixedPrice' => 'Yes', 'name' => '60GB (CG) - N1351648800 30days', 'varition_amount' =>],
                      [
-                        'variation_code' => 144, 'fixedPrice' => 'Yes', 'name' => '40GB (CG) - N901099200 30days', 'varition_amount' =>],
+                        'variation_code' => 147, 
+                        'fixedPrice' => 'Yes', 
+                        'name' => '70GB (CG) - 30days', 
+                        'varition_amount' => 1576923600
+                    ],
+                    [
+                        'variation_code' => 148, 
+                        'fixedPrice' => 'Yes', 
+                        'name' => '75GB (CG) -  30days', 
+                        'varition_amount' => 1689561000
+                    ],
                      [
-                        'variation_code' => 145, 'fixedPrice' => 'Yes', 'name' => '50GB (CG) - N1126374000 30days', 'varition_amount' =>],
+                        'variation_code' => 149, 
+                        'fixedPrice' => 'Yes', 
+                        'name' => '80GB (CG) - 30days', 
+                        'varition_amount' => 1802198400
+                    ],
                      [
-                        'variation_code' => 146, 'fixedPrice' => 'Yes', 'name' => '60GB (CG) - N1351648800 30days', 'varition_amount' =>],
-                     [
-                        'variation_code' => 147, 'fixedPrice' => 'Yes', 'name' => '70GB (CG) - N1576923600 30days', 'varition_amount' =>],
-                     [
-                        'variation_code' => 148, 'fixedPrice' => 'Yes', 'name' => '75GB (CG) - N1689561000 30days', 'varition_amount' =>],
-                     [
-                        'variation_code' => 149, 'fixedPrice' => 'Yes', 'name' => '80GB (CG) - N1802198400 30days', 'varition_amount' =>],
-                     [
-                        'variation_code' => 150, 'fixedPrice' => 'Yes', 'name' => '90GB (CG) - N2027473200 30days', 'varition_amount' =>],
-                     [
+                        'variation_code' => 150, 
+                        'fixedPrice' => 'Yes', 
+                        'name' => '90GB (CG) - 30days', 
+                        'varition_amount' => 2027473200
+                    ],
+                    [
                         'variation_code' => 151, 
                         'fixedPrice' => 'Yes', 
                         'name' => '100GB (CG) - 30days', 
                         'varition_amount' => 2252748000],
                     [
-                        'variation_code' => 152, 'fixedPrice' => 'Yes', 'name' => '750MB (Direct) - N47262500 14days'
-                    ', varition_amount' =>],
-                     [
-                        'variation_code' => 83, 'fixedPrice' => 'Yes', 'name' => '1.5GB (Direct) - N965 .15 30days],
-                     ],
-
+                        'variation_code' => 152, 
+                        'fixedPrice' => 'Yes', 
+                        'variation_amount' => 47262500,
+                        'name' => '750MB (Direct) - 14days'
+                    ],
+                    [
+                        'variation_code' => 83, 
+                        'fixedPrice' => 'Yes', 
+                        'name' => '1.5GB (Direct) - 30days',
+                        'variation_amount' => 965
+                    ],
                     //  [
                     //     'variation_code' => 84, 'fixedPrice' => 'Yes', 'name' => '2GB (Direct) - N1158 18' 30days],
                     //  [
@@ -251,8 +270,14 @@ class EasyAccessController extends Controller
                     //     'variation_code' => 103, 'fixedPrice' => 'Yes', 'name' => '75GB (Direct) - N15451.355' 30days],
                     //  [
                     //     'variation_code' => 155, 'fixedPrice' => 'Yes', 'name' => '120GB (Direct) - N21233.3' 30days, 156 => 200GB (Direct) - N28954.5 30days.],
+                ],
+            ],
+            'airtel-data' => [
+                'variations' => [
+
+                ],
             ]
-        ]
+        ];
     }
 
 }
