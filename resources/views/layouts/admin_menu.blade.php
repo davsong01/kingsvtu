4acddc90a8fbaa875ab854da6e6610fba88719e2
davsong01 <davsong16@gmail.com>
2024-03-02 14:24:04 +0100

    admin

--- resources/views/layouts/admin_menu.blade.php
+++ resources/views/layouts/admin_menu.blade.php
@@ -26,11 +26,7 @@
                      </svg><span class="menu-title" data-i18n="Form Layout">&nbsp;Dashboard</span></a>
              </li>
              <li class="{{ Request::path() == 'profile' ? 'active' : '' }}"><a
-                href="{{ route('announcement.index') }}"><svg xmlns="http://www.w3.org/2000/svg" height="24"
-                    fill="white" viewBox="0 -960 960 960" width="24">
-                    <path
-                        d="M200-246q54-53 125.5-83.5T480-360q83 0 154.5 30.5T760-246v-514H200v514Zm280-194q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm80-80h400v-10q-42-35-93-52.5T480-280q-56 0-107 17.5T280-210v10Zm200-320q-25 0-42.5-17.5T420-580q0-25 17.5-42.5T480-640q25 0 42.5 17.5T540-580q0 25-17.5 42.5T480-520Zm0 17Z" />
-                </svg><span class="menu-title">&nbsp;Announcement</span></a></li>
+                href="{{ route('announcement.index') }}"><svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg><span class="menu-title">&nbsp;Announcement</span></a></li>
              <li class="nav-item"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24"
                          viewBox="0 -960 960 960" width="24">
                          <path
