76c7adb46c53bce732ff2d44e700e6886c724b2b
davsong01 <davsong16@gmail.com>
2026-01-13 13:49:13 +0100

    Updates

--- app/Http/Controllers/Providers/MobileNigController.php
+++ app/Http/Controllers/Providers/MobileNigController.php
@@ -73,9 +73,15 @@ class MobileNigController extends Controller
 
             $response = $this->basicApiCall($url, json_encode($payload), $headers, 'POST');
 
-            // \Log::info('MobileNigController');
-            // \Log::info(['payload' => $payload, 'url' => $url]);
-            // \Log::info(['response' => $response]);
+            \Log::info(
+                json_encode([
+                    'endpoint' => $url,
+                    'headers' => $headers,
+                    'payload' => $payload,
+                    'response' => $response, 
+                ])
+            );
+            
             // if($product->slug == 'data'){
             // }
             if (isset($response['statusCode']) && in_array($response['statusCode'], ['200', '201','202']) && $response['message'] == 'success') {
