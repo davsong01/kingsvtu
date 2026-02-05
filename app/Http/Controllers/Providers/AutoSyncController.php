b9fb9a70b2e2d8d9e3e9471d0fbf3249dff74f67
davsong01 <davsong16@gmail.com>
2026-02-05 10:39:06 +0100

    Updates

--- app/Http/Controllers/Providers/AutoSyncController.php
+++ app/Http/Controllers/Providers/AutoSyncController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 
+use function PHPUnit\Framework\stringContains;
+
 class AutoSyncController extends Controller
 {
     public function getAirtimeOptions(){
@@ -58,164 +60,73 @@ class AutoSyncController extends Controller
 
     public function getVariations($product)
     {
-        $url = $product->api->live_base_url;
-        $url = $url . "get/data/plans";
-        
+        $api = $product->api;
+        $baseUrl = $api->live_base_url;
+
+        $slug = $product->slug;
+
+        $specificUrl = match (true) {
+            str_contains($slug, 'data')   => 'data',
+            // str_contains($slug, 'direct') => 'data/transfer',
+            default => null,
+        };
+
+        $url = $baseUrl . $specificUrl;
+
+
         $headers = [
-            'Content-Type' => 'application/json',
-            'Accept' =>  'application/json',
-            'Authorization: Bearer ' . $product->api->api_key,
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $api->api_key,
         ];
-
-        // $variations = $this->basicApiCall($url, [], $headers, 'GET');
-        $variations = $this->staticVariations();
         
-        // mtn sme
-        $mtnSmePlanIds = [1, 2, 3, 4, 5, 109];
-        $mtnCgPlanIds = [94, 95, 96, 97, 98, 99, 101, 102, 103, 104, 105, 106, 107, 108, 940, 950, 960, 970, 980, 990, 1010, 1020, 1030, 1040, 1060, 1070, 1080, 11110, 11111, 11112, 800, 801, 802, 803, 804, 805, 806, 807, 7600, 7601, 7602, 7603, 7604, 7605, 7606, 7607, 7608, 12000, 12001, 12002, 12003, 12004, 12005, 12006, 12007, 12008, 12009, 12010];
-        $mtnGiftingPlanIds = [10000, 10001, 10002, 10003, 10004, 10005, 10006, 10007, 10008, 10009, 10010, 10011, 10012, 10013, 10014, 10015, 10016, 10017, 10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 10033, 10034, 10035, 10036, 10037, 10038, 10039, 10040, 10041, 10042, 10043];
+        $res = $this->basicApiCall($url, [], $headers, 'GET');
 
-        $mtnAwoofPlanIds = [11113, 11114, 11115];
-        $airtelAwoofPlanIds = [9000, 9001, 9002, 9003, 9004, 9005, 9006, 9007];
-        
-        if(!empty($variations)){
+        if($res['status'] && $res['status'] == 'ok' && $res['data']['category']['products'] && !empty($res['data']['category']['products'])){
+            $products = collect($res['data']['category']['products']);
+           
+            $codeToSearch = match ($slug) {
+                'mtn-data' => 'mtn',
+                'mtn-direct' => 'mtn',
+                'glo-data' => 'glo',
+                'airtel-data' => 'airtel',
+                '9mobile-data' => '9mobile',
+                default => null,
+            };
+               
+            $apiProduct = $products->where('code', $codeToSearch)->first();
+            $variations = $apiProduct['variations'] ?? [];
+            if(empty($variations)) return false;
+
             foreach ($variations as $variation) {
-                // Mtn SME
-                if (in_array($product->slug, ['mtn-sme']) && in_array($variation['planId'], $mtnSmePlanIds) && $variation['networkId'] == 1) {
-                    Variation::updateOrCreate([
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'api_code' => $variation['planId'],
-                        'slug' => $variation['planId'],
-                    ], [
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'slug' => $variation['planId'],
-                        'api_code' => $variation['planId'],
-                        'system_name' => $variation['name'],
-                        'fixed_price' => 'Yes',
-                        'api_price' => $variation['price'],
-                        'system_price' => $variation['price'],
-                        'min' => $variation['minimum_amount'] ?? null,
-                        'max' => $variation['maximum_amount'] ?? null,
-                        'status' => 'inactive'
-                    ]);
-                }
-
-                // Mtn cg
-                if(in_array($product->slug, ['mtn-cg', 'mtn-cg-data']) && in_array($variation['planId'], $mtnCgPlanIds) && $variation['networkId'] == 1 ){
-                    Variation::updateOrCreate([
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'api_code' => $variation['planId'],
-                        'slug' => $variation['planId'],
-                    ], [
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'slug' => $variation['planId'],
-                        'api_code' => $variation['planId'],
-                        'system_name' => $variation['name'],
-                        'fixed_price' => 'Yes',
-                        'api_price' => $variation['price'],
-                        'system_price' => $variation['price'],
-                        'min' => $variation['minimum_amount'] ?? null,
-                        'max' => $variation['maximum_amount'] ?? null,
-                        'status' => 'inactive'
-                    ]);
-                }
-
-                // Mtn Awoof
-                if (in_array($product->slug, ['mtn-awoof']) && in_array($variation['planId'], $mtnAwoofPlanIds) && $variation['networkId'] == 1) {
-                    Variation::updateOrCreate([
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'api_code' => $variation['planId'],
-                        'slug' => $variation['planId'],
-                    ], [
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'slug' => $variation['planId'],
-                        'api_code' => $variation['planId'],
-                        'system_name' => $variation['name'],
-                        'fixed_price' => 'Yes',
-                        'api_price' => $variation['price'],
-                        'system_price' => $variation['price'],
-                        'min' => $variation['minimum_amount'] ?? null,
-                        'max' => $variation['maximum_amount'] ?? null,
-                        'status' => 'inactive'
-                    ]);
-                }
-
-                // Airtel Awoof
-                if (in_array($product->slug, ['airtel-awoof', 'airtel-awoof-data']) && in_array($variation['planId'], $airtelAwoofPlanIds) && $variation['networkId'] == 2) {
-                    Variation::updateOrCreate([
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'api_code' => $variation['planId'],
-                        'slug' => $variation['planId'],
-                    ], [
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'slug' => $variation['planId'],
-                        'api_code' => $variation['planId'],
-                        'system_name' => $variation['name'],
-                        'fixed_price' => 'Yes',
-                        'api_price' => $variation['price'],
-                        'system_price' => $variation['price'],
-                        'min' => $variation['minimum_amount'] ?? null,
-                        'max' => $variation['maximum_amount'] ?? null,
-                        'status' => 'inactive'
-                    ]);
-                }
-            
-                // Mtn Gifting
-                if (in_array($product->slug, ['mtn-gifting','mtn-gifting-data']) && in_array($variation['planId'], $mtnGiftingPlanIds) && $variation['networkId'] == 1) {
-                    Variation::updateOrCreate([
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'api_code' => $variation['planId'],
-                        'slug' => $variation['planId'],
-                    ], [
-                        'product_id' => $product['id'],
-                        'category_id' => $product['category_id'],
-                        'api_id' => $product['api']['id'],
-                        'api_name' => $variation['name'],
-                        'slug' => $variation['planId'],
-                        'api_code' => $variation['planId'],
-                        'system_name' => $variation['name'],
-                        'fixed_price' => 'Yes',
-                        'api_price' => $variation['price'],
-                        'system_price' => $variation['price'],
-                        'min' => $variation['minimum_amount'] ?? null,
-                        'max' => $variation['maximum_amount'] ?? null,
-                        'status' => 'inactive'
-                    ]);
-                }
-
+                Variation::updateOrCreate([
+                    'product_id' => $product['id'],
+                    'category_id' => $product['category_id'],
+                    'api_id' => $product['api']['id'],
+                    'api_name' => $variation['name'],
+                    'api_code' => $variation['code'],
+                    'slug' => $variation['code'],
+                ], [
+                    'product_id' => $product['id'],
+                    'category_id' => $product['category_id'],
+                    'api_id' => $product['api']['id'],
+                    'api_name' => $variation['name'],
+                    'slug' => $variation['code'],
+                    'api_code' => $variation['code'],
+                    'system_name' => $variation['name'],
+                    'fixed_price' => 'Yes',
+                    'api_price' => $variation['amount'],
+                    'system_price' => $variation['amount'],
+                    'min' => $variation['minimum_amount'] ?? null,
+                    'max' => $variation['maximum_amount'] ?? null,
+                    'status' => 'inactive'
+                ]);
 
             }
             return true;
-        }else {
+        }else{
             return false;
         }
+        
     }
 
     function query($request, $api, $variation, $product)
@@ -280,7 +191,7 @@ class AutoSyncController extends Controller
             ];
 
             $payloadJson = json_encode($payload);
-
+            
             // $res = env('ENT') == 'local'
             //     ? json_decode($this->dummySuccess(), true)
             //     : $this->basicApiCall($url, $payloadJson, $headers, 'POST');
